d0344abf2b4590abcb184f95d618e630ca59a5d8
Alexander de Ranitz <a.deranitz@roboteamtwente.nl>
2021-09-29 13:52:01 +0200

    Do not get too close to ball if robot is not facing forward
    In GetBall, the robot would rotate and move towards the ball simultaneously. However, if the ball is close to the robot, it would not be done rotating before already getting to the ball, which caused it to sometimes hit the ball with the side of the robot. By ensuring that the robot is facing the right direction before getting into dribbling range, it is made sure that the robot gets the ball with the dribbler and not hit it with the side.

--- src/stp/tactics/active/GetBall.cpp
+++ src/stp/tactics/active/GetBall.cpp
@@ -22,23 +22,30 @@ std::optional<StpInfo> GetBall::calculateInfoForSkill(StpInfo const &info) noexc
 
     Vector2 robotPosition = info.getRobot().value()->getPos();
     Vector2 ballPosition = info.getBall().value()->getPos();
+    double ballDistance = (ballPosition - robotPosition).length();
 
     // If this robot is not the keeper, don't get the ball inside a defense area
     if (info.getRobot()->get()->getId() != GameStateManager::getCurrentGameState().keeperId && FieldComputations::pointIsInDefenseArea(info.getField().value(), ballPosition)) {
         ballPosition = control::ControlUtils::projectPositionToOutsideDefenseArea(info.getField().value(), ballPosition, control_constants::AVOID_BALL_DISTANCE);
     }
 
-    // the robot will go to the position of the ball
-    double ballDistance = (ballPosition - robotPosition).length();
-    Vector2 newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - control_constants::CENTER_TO_FRONT + 0.035);
+    if (skillStpInfo.getRobot()->get()->getAngle().shortestAngleDiff((ballPosition - robotPosition).angle()) > control_constants::HAS_BALL_ANGLE_ERROR_MARGIN/2
+        && ballDistance < control_constants::AVOID_BALL_DISTANCE){
+        // don't move too close to the ball until the angle to the ball is correct
+        skillStpInfo.setPositionToMoveTo(skillStpInfo.getRobot()->get()->getPos());
+    }
+    else{
+        // the robot will go to the position of the ball
+        Vector2 newRobotPosition = robotPosition + (ballPosition - robotPosition).stretchToLength(ballDistance - control_constants::CENTER_TO_FRONT + 0.035);
+        skillStpInfo.setPositionToMoveTo(newRobotPosition);
+    }
+
 
     skillStpInfo.setAngle((ballPosition - robotPosition).angle());
 
     if (ballDistance < control_constants::TURN_ON_DRIBBLER_DISTANCE) {
         skillStpInfo.setDribblerSpeed(100);
     }
-
-    skillStpInfo.setPositionToMoveTo(newRobotPosition);
 
     return skillStpInfo;
 }
